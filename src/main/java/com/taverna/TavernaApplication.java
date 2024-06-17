d57a75ad5aa397f3df2c19304707f27671c1aa61
AllanSeidler <allanseidler@gmail.com>
2024-06-17 16:44:44 -0300

    Esqueleto criado
    Esqueleto do programa criado usando https://start.spring.io/

---
+++ src/main/java/com/taverna/TavernaApplication.java
@@ -0,0 +1,13 @@
+package com.taverna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavernaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taverna/TavernaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taverna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
